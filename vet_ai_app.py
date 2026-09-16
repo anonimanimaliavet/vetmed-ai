080f34cf1209d0483024046fb28b3bdde76beb15
anonimanimaliavet <vetardaozocak@gmail.com>
2026-09-16 20:11:53 +0300

    Add files via upload

--- vet_ai_app.py
+++ vet_ai_app.py
@@ -262,7 +262,7 @@ def veritabani_baglanti_ve_kontrol():
 
 aktif_vaka_sayisi = veritabani_baglanti_ve_kontrol()
 
-SECURE_GEMINI_API_KEY = "AQ.Ab8RN6I-P79ziUatwOiVJzIOgfzttkRXelkZ4lA_oRSh34YhUw" 
+SECURE_GEMINI_API_KEY = "AQ.Ab8RN6LMUjQziHwdZ-sZd3VVln6PrUoQI1Nbdqxem0HL0smz_A" 
 
 def guvenlik_dogrula(kullanici_input, sifre_input):
     if not kullanici_input or not sifre_input:
